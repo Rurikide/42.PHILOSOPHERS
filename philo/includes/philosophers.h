c64e54cbd76133222c3d261851ef2f4f6dc9cd6d
Tokutaro Shimoda <tshimoda@c2r3p8.42quebec.com>
2022-01-27 12:20:05 -0500

    1

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: tshimoda <tshimoda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 19:22:15 by tshimoda          #+#    #+#             */
-/*   Updated: 2022/01/26 16:16:45 by tshimoda         ###   ########.fr       */
+/*   Updated: 2022/01/27 12:16:17 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,17 @@
 
 # include <pthread.h>
 # include <stdio.h>
+# include <stdlib.h>
 # include <sys/time.h>
 # include <unistd.h>
 
+typedef pthread_mutex_t		mutex_t;
+
 typedef enum e_result
 {
-	fail = -1,
+	unavailable = -1,
 	false = 0,
-	true = 1
+	true = 1,
 }	t_result;
 
 typedef enum e_fork
@@ -41,11 +44,11 @@ typedef enum e_act
 
 typedef struct s_philo
 {
-	struct t_container	*data;
+	pthread_mutex_t		mutex_fork;
+	struct t_container	*cont;
 	int					id;
 	int					spag_plate;
 	t_act				act;
-	pthread_mutex_t		mutex_fork;
 }	t_philo;
 
 typedef struct s_param
@@ -61,11 +64,19 @@ typedef	struct s_container
 {
 	pthread_mutex_t	print;
 	pthread_mutex_t	sudden_death;
-	t_philo			*philo;
 	t_param			*param;
+	t_philo			*philo;
+	int				*queue;
+	int				odd_one;
+	long long		start_time;
 }	t_container;
 
 t_result	bool_isdigit(char *str);
 t_result	bool_is_valid_input(int	ac, char **ag);
 int			ft_atoi(const char *str);
+
+void	init_container(t_container *cont, int ac, char **av);
+t_param	*set_parameters(int ac, char **av);
+t_philo *get_philosophers(t_container cont);
+
 #endif
--- philo/sources/main.c
+++ philo/sources/main.c
@@ -6,7 +6,7 @@
 /*   By: tshimoda <tshimoda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 19:21:25 by tshimoda          #+#    #+#             */
-/*   Updated: 2022/01/26 16:30:16 by tshimoda         ###   ########.fr       */
+/*   Updated: 2022/01/27 12:17:56 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,22 +27,29 @@ t_result	bool_is_valid_input(int ac, char **av)
 	return (true);
 }
 
-void	ft_parse_command_line(t_param *set, int ac, char **av)
+t_param	*set_parameters(int ac, char **av)
 {
-	set->nb_philo = ft_atoi(av[1]);
-	set->tt_die = ft_atoi(av[2]);
-	set->tt_eat = ft_atoi(av[3]);
-	set->tt_sleep = ft_atoi(av[4]);
-	printf("nb_philo = %d\n", set->nb_philo);
-	printf("time to die = %ld\n", set->tt_die);
-	printf("time to eat = %ld\n", set->tt_eat);
-	printf("time to sleep = %ld\n", set->tt_sleep);
+	t_param *param;
+
+	param = (t_param *)malloc(sizeof(t_param));
+	param->nb_philo = ft_atoi(av[1]);
+	param->tt_die = ft_atoi(av[2]);
+	param->tt_eat = ft_atoi(av[3]);
+	param->tt_sleep = ft_atoi(av[4]);
+	printf("nb_philo = %d\n", param->nb_philo);
+	printf("time to die = %ld\n", param->tt_die);
+	printf("time to eat = %ld\n", param->tt_eat);
+	printf("time to sleep = %ld\n", param->tt_sleep);
 
 	if (ac == 6)
 	{
-		set->nb_serving = ft_atoi(av[5]);
-		printf("nb of serving = %d\n", set->nb_serving);
+		param->nb_serving = ft_atoi(av[5]);
+		printf("nb of serving = %d\n", param->nb_serving);
 	}
+	else 
+		param->nb_serving = unavailable;
+
+	return (param);
 }
 
 void    *test()
@@ -61,19 +68,29 @@ void    *test()
 //	pthread_join(p1, NULL);
 //}
 
+void	init_container(t_container *cont, int ac, char **av)
+{
+
+	pthread_mutex_init(&cont->print, NULL);
+	pthread_mutex_init(&cont->sudden_death, NULL);
+	cont->param = set_parameters(ac, av);
+	cont->philo = get_philosophers(cont);
+}
+
 int main(int ac, char **av)
 {
-	t_param	set;
-	if (ac == 5 || ac == 6)
+	t_container cont;
+
+	if (ac < 5 || ac > 6)
 	{
-		if (bool_is_valid_input(ac, av) == false)
-		{
-			printf("Error : Non digit argument\n");
-			return (0);
-		}
-		ft_parse_command_line(&set, ac, av);
-	}
-	else
 		printf("Error incorrect input. Enter: ./philo [nb_of_philosophers] [time_to_die] [time_to_eat] [time_to_sleep] :::optional::: [nb_of_serving]\n");
+    	return (0);
+	}
+	if (bool_is_valid_input(ac, av) == false)
+	{
+		printf("Error : Parameter containing non-digit character found\n");
+		return (0);
+	}
+	init_container(&cont, ac, av);
     return (0);
 }
